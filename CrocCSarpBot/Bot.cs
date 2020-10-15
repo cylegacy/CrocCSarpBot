71ccd55ea98b474788912576a7bf7b851f17bdd0
Кирилл Наследников <knaslednikov@croc.ru>
2020-10-15 20:59:06 +0300

    Добавлено протоколирование через  NLog + обработчик исключений

--- CrocCSarpBot/Bot.cs
+++ CrocCSarpBot/Bot.cs
@@ -14,14 +14,24 @@ namespace CrocCSarpBot
     /// </summary>
     class Bot
     {
+        /// <summary>
+        /// Клиент Telegram
+        /// </summary>
         private TelegramBotClient client;
+        /// <summary>
+        /// Ведение журнала событий
+        /// </summary>
+        private NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
         /// <summary>
         /// Конструктор без параметров
         /// </summary>
         public Bot()
         {
             // Создание клиента для Telegram
-            client = new TelegramBotClient("1143399443:AAF-umACGpif8YAN05GBcYpuhPXHYK7yvIc");
+            var token = Properties.Settings.Default.Token;
+            client = new TelegramBotClient(token);
+            var user = client.GetMeAsync();
+            var userResult = user.Result;
             client.OnMessage += MessageProcessor;
         }
 
@@ -56,86 +66,117 @@ namespace CrocCSarpBot
         /// <param name="e"></param>
         private void MessageProcessor(object sender, MessageEventArgs e)
         {
-            switch (e.Message.Type)
+            try
             {
-                case Telegram.Bot.Types.Enums.MessageType.Unknown:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Text:
-                    if (e.Message.Text.Substring(0, 1) == "/")
-                    {
-                        CommandProcessor(e.Message);
-                    }
-                    else
-                    {
-                        client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты мне сказал: {e.Message.Text}");
-                        Console.WriteLine(e.Message.Text);
-                    }
-                   
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Photo:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Audio:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Video:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Voice:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Document:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Sticker:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Location:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Contact:
-                    var phone = e.Message.Contact.PhoneNumber;
-                    client.SendTextMessageAsync(e.Message.Chat.Id, $"Твой телефон: {phone}");
-                    Console.WriteLine(phone);
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Venue:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Game:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.VideoNote:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Invoice:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.SuccessfulPayment:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.WebsiteConnected:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChatMembersAdded:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChatMemberLeft:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChatTitleChanged:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChatPhotoChanged:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.MessagePinned:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChatPhotoDeleted:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.GroupCreated:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.SupergroupCreated:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.ChannelCreated:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.MigratedToSupergroup:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.MigratedFromGroup:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Animation:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Poll:
-                    break;
-                case Telegram.Bot.Types.Enums.MessageType.Dice:
-                    break;
-                default:
-                    client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты прислал мне: {e.Message.Type}");
-                    Console.WriteLine(e.Message.Text);
-                    break;
+
+
+                _log.Trace("|<- MessageProcessor");
+                switch (e.Message.Type)
+                {
+                    case Telegram.Bot.Types.Enums.MessageType.Unknown:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Text:
+                        if (e.Message.Text.Substring(0, 1) == "/")
+                        {
+                            CommandProcessor(e.Message);
+                        }
+                        else
+                        {
+                            client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты мне сказал: {e.Message.Text}");
+                            _log.Trace(e.Message.Text);
+                        }
+
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Photo:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Audio:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Video:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Voice:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Document:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Sticker:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Location:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Contact:
+                        var phone = e.Message.Contact.PhoneNumber;
+                        client.SendTextMessageAsync(e.Message.Chat.Id, $"Твой телефон: {phone}");
+                        _log.Trace(phone);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Venue:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Game:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.VideoNote:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Invoice:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.SuccessfulPayment:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.WebsiteConnected:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChatMembersAdded:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChatMemberLeft:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChatTitleChanged:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChatPhotoChanged:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.MessagePinned:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChatPhotoDeleted:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.GroupCreated:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.SupergroupCreated:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.ChannelCreated:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.MigratedToSupergroup:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.MigratedFromGroup:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Animation:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Poll:
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Dice:
+                        var diceMessage = client.SendDiceAsync(e.Message.Chat.Id).Result;
+                        if (e.Message.Dice.Value > diceMessage.Dice.Value)
+                        {
+                            client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты выиграл!");
+                        }
+
+                        if (e.Message.Dice.Value < diceMessage.Dice.Value)
+                        {
+                            client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты проиграл(");
+                        }
+
+                        if (e.Message.Dice.Value == diceMessage.Dice.Value)
+                        {
+                            client.SendTextMessageAsync(e.Message.Chat.Id, $"Ничья. Кидай еще раз!");
+                        }
+
+                        break;
+                    default:
+                        client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты прислал мне: {e.Message.Type}. Я такой тип не понимаю.");
+                        _log.Info(e.Message.Text);
+                        break;
+                }
             }
+            catch (Exception ex)
+            {
+                _log.Warn(ex);
+            }
+            finally
+            {
+
+            }
+            _log.Trace("|-> MessageProcessor");
         }
 
         public void Run()
--- CrocCSarpBot/Program.cs
+++ CrocCSarpBot/Program.cs
@@ -6,22 +6,47 @@ using System.Threading.Tasks;
 
 namespace CrocCSarpBot
 {
+
+
     /// <summary>
     /// Главный класс приложения
     /// </summary>
     class Program
     {
+
+        /// <summary>
+        /// Ведение журнала событий
+        /// </summary>
+        private static NLog.Logger _log = NLog.LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// Точка входа в приложение
         /// </summary>
         /// <param name="args">Параметры командной строки</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Запуск бота в консольном режиме. Нажмите Enter для завершения.");
-            Bot bot;
-            bot = new Bot();
-            bot.Run();
-            Console.ReadLine();
+            try
+            {
+                var bot = new Bot();
+                bot.Run();
+                _log.Info("Запуск бота в консольном режиме.");
+            }
+            catch (Exception e)
+            {
+                // Отображание исключения, включая все вложенные исключения
+                do
+                {
+                    Console.WriteLine(e.Message);
+                    e = e.InnerException;
+                } while (e != null);
+                
+            }
+            finally
+            {
+                _log.Info("Нажмите Enter для завершения.");
+                Console.ReadLine();
+            }
+
         }
     }
 }
